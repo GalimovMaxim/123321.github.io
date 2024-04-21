4de386fc3e59712c7fe0cfef8e4a004491b3b557
GalimovMaxim <162039054+GalimovMaxim@users.noreply.github.com>
2024-04-21 18:33:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function fun2() {
+    alert("Здесь представлены события на этот празднычный день.");
+    text2 = document.querySelector("#info")
+    text2.innerHTML = 'День Весны и Труда. Этот день считается символом весны, он олицетворяет собой уважение к любому созидательному труду. Традиция встречать этот праздник насчитывает уже много лет.'
+    
+
+}
+
+function fun3() {
+    alert("Здесь представлены события на этот празднычный день.");
+    text3 = document.querySelector("#info")
+    text3.innerHTML = 'День Победы. Праздник победы Красной армии и советского народа над нацистской Германией в Великой Отечественной войне 1941—1945 годов.'
+    img=document.querySelector("#photo")
+    img.style.src="Kiprensky_Pushkin.jpg"
+
+
+}
+
+but2 = document.querySelector("#m1")
+but2.addEventListener('click', fun2)
+
+
+but2 = document.querySelector("#m9")
+but2.addEventListener('click', fun3)
+labek.addEventListener("")
